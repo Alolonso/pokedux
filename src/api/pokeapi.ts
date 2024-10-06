d3cc05aae58fe25732cbe05f8b188230a423351b
Alolonso <alonsodr11@gmail.com>
2024-10-05 18:26:41 -0600

    Implement function to fetch Pokémon details from the API

--- src/api/pokeapi.ts
+++ src/api/pokeapi.ts
@@ -17,6 +17,24 @@ type GetPokemonListType =
       error: string
     }
 
+interface PokemonTypeApiResponseType {
+  type: {
+    name: string
+  }
+}
+
+interface PokemonDetailedApiResponseType {
+  name: string
+  sprites: {
+    front_default: string
+  }
+  types: PokemonTypeApiResponseType[]
+}
+
+type GetPokemonDetailedType =
+  | (PokemonDetailedType & { error?: undefined })
+  | (Partial<PokemonDetailedType> & { error: string })
+
 const pokeapi = axios.create({
   baseURL: 'https://pokeapi.co/api/v2/',
   timeout: 5000
@@ -34,6 +52,26 @@ export const getPokemonList = async (
     return { count: data.count, results: data.results }
   } catch (error) {
     console.error('Error fetching Pokémon list:', error)
+    return {
+      error: 'There was an issue retrieving the data. Please try again later'
+    }
+  }
+}
+
+export const getPokemonDetails = async (
+  pokemon: PokemonType
+): Promise<GetPokemonDetailedType> => {
+  try {
+    const { data } = await pokeapi.get<PokemonDetailedApiResponseType>(
+      pokemon.url
+    )
+
+    return {
+      name: data.name,
+      image: data.sprites.front_default,
+      types: data.types.map(({ type }) => type.name)
+    }
+  } catch (error) {
     return {
       error: 'There was an issue retrieving the data. Please try again later'
     }
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,3 +2,9 @@ interface PokemonType {
   name: string
   url: string
 }
+
+interface PokemonDetailedType {
+  name: string
+  image: string | null
+  types: string[]
+}
